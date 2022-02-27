e30b4b21e19d6324397b2711cb64c1288fd301ab
Romain Coupey <schaft115@hotmail.com>
2022-02-27 22:00:55 +0100

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,95 +23,100 @@ seed();
 function getPlaces() {
   return [
     {
-      name: "Boulangerie Saint Ambroise",
+      name: "Boulangerie Patisserie by KR",
       content: `Sandwich`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8602276",
+      lng: "2.3757439",
+      address:
+        "90, Boulevard Voltaire, Quartier de la Folie-Méricourt, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Bombolo",
       content: `Pizza & Pasta`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8602276",
+      lng: "2.3757439",
+      address:
+        "90, Boulevard Voltaire, Quartier de la Folie-Méricourt, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Chez Colette",
       content: `Plats & Sandwich`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8611455",
+      lng: "2.3778135",
+      address:
+        "Avenue Parmentier, Quartier Saint-Ambroise, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Fresh Bagel & Juice",
       content: `Sandwich`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8565378",
+      lng: "2.3733225",
+      address:
+        "1, Rue Froment, Quartier de la Roquette, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Ten Belles Bread",
       content: `Sandwich`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8575489",
+      lng: "2.3734779",
+      address:
+        "Ten Belles Bread, 17-19, Rue Bréguet, Quartier de la Roquette, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Cafe des Anges",
       content: `Plats`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8554359",
+      lng: "2.3744522",
+      address:
+        "Le Café des Anges, 66, Rue de la Roquette, Quartier de la Roquette, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Ebisu",
       content: `Sushi`,
-      lat: "",
-      lng: "",
-      address: "",
-    },
-    {
-      name: "Le Grand Breguet",
-      content: `Plats`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8585125",
+      lng: "2.3733329",
+      address:
+        "Poissonnerie EBISU, 30-34, Rue du Chemin Vert, Quartier de la Roquette, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Pita Street Food",
       content: `Sandwich`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8543623",
+      lng: "2.3716748",
+      address:
+        "34, Rue de la Roquette, Quartier de la Roquette, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Chez Aline",
       content: `Plats & Sandwich`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8570961",
+      lng: "2.3784567",
+      address:
+        "Chez Aline, 85, Rue de la Roquette, Quartier de la Roquette, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Le Poulailler",
       content: `Plats`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8591081",
+      lng: "2.3688621",
+      address:
+        "60, Rue Saint-Sabin, Quartier de la Roquette, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "La vache acrobate",
       content: `Plats`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.8596492",
+      lng: "2.3680697",
+      address:
+        "77, Rue Amelot, Quartier de la Folie-Méricourt, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
     {
       name: "Mokoloco",
       content: `Burger`,
-      lat: "",
-      lng: "",
-      address: "",
+      lat: "48.853432",
+      lng: "2.3800126",
+      address:
+        "Mokoloco, 74, Rue de Charonne, Quartier Sainte-Marguerite, 11th Arrondissement, Paris, Ile-de-France, Metropolitan France, 75011, France",
     },
   ];
 }
